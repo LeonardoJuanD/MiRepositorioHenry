748f880ecee9d706eb1c0a3220726760a12f7c91
LeonardoJuanD <goz.cba.cs@gmail.com>
2024-04-12 09:52:05 -0300

    Clase de JavaScript

---
+++ CarpetaHenry/C5 JavaScript/funciones.js
@@ -0,0 +1,22 @@
+//Función: sumaTres
+//var sumaTres = (x) => {
+//    console.log(x + 3);
+//}
+//  sumaTres(5)
+
+//PRIMERO
+function sumaTres(x){
+    return x + 3;
+}
+
+//SEGUNDO
+
+var sumaTres = function(x){
+    return x + 3;
+}
+
+//TERCERO
+
+var sumaTres = (x) => {
+    return x + 3;
+}
---
+++ CarpetaHenry/C5 JavaScript/operadores.js
@@ -0,0 +1,14 @@
+// Suma
+console.log(3+5);
+// Resta
+console.log(14-7);
+// Multiplicación
+console.log(2*3);
+// División
+console.log(30/2);
+// Módulo o Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
